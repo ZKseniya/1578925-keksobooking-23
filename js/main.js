6340468fb13d7b9c1d1de2a16ae6326bb0d8143b
ZKseniya <zaharina.ksenia@yandex.ru>
2021-05-30 20:25:40 +0300

    fix: исправляет ошибки в коде с учетом замечаний наставника

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-function getRandomArbitrary(firstNumber, secondNumber, numberOfDemicalPlace) {
-  let number = Math.random();
+function getRandomArbitrary(firstNumber, secondNumber, numberOfDemicalPlace = 0) {
+
+  if (firstNumber === undefined || secondNumber === undefined) {
+    throw new Error('Ошибка! Диапазон не определен. Нужно ввести два числа');
+  }
 
   if (firstNumber < 0 || secondNumber < 0) {
     throw new Error('Ошибка! Диапазон не должен содержать отрицательные числа.');
+  }
+
+  if (numberOfDemicalPlace < 0) {
+    throw new Error('Ошибка! Количество знаков после запятой не может быть отрицательным.');
   }
 
   if (firstNumber === secondNumber) {
     throw new Error('Ошибка! Числа не должны быть равны.');
   }
 
-  number = (Math.round(number * Math.pow(10, numberOfDemicalPlace)) / Math.pow(10, numberOfDemicalPlace)) * (secondNumber - firstNumber) + firstNumber;
+  let number = Math.random();
 
-  return number.toFixed(numberOfDemicalPlace);
+  if (numberOfDemicalPlace < 2) {
+    number = (Math.round(number * 100)) / 100 * (secondNumber - firstNumber) + firstNumber;
+  } else {
+    number = (Math.round(number * Math.pow(10, numberOfDemicalPlace)) / Math.pow(10, numberOfDemicalPlace)) * (secondNumber - firstNumber) + firstNumber;
+  }
+
+  return parseFloat(number.toFixed(numberOfDemicalPlace));
 }
 
-getRandomArbitrary(5.555, 0, 5);
+getRandomArbitrary(99, 2, 1);
