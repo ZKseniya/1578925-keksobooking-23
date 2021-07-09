0a94d15a6245208fc1e1ea6286da4363bb3bdf7f
ZKseniya <zaharina.ksenia@yandex.ru>
2021-07-09 17:18:41 +0300

    feat: добавляет валидацию формы

--- js/form-processing.js
+++ js/form-processing.js
@@ -17,16 +17,44 @@ const addFormsLocking = () => {
 
 const removeFormsLocking = () => {
   const removeDisabled = (element) => {
-    if (element.classList.contains(`${element.classList.item(0)  }--disabled`) === true) {
-      element.classList.remove(`${element.classList.item(0)  }--disabled`);
+    element.classList.remove(`${element.classList.item(0)  }--disabled`);
 
-      element.childNodes.forEach((item) => {
-        item.disabled = false;
-      });
-    }
+    element.childNodes.forEach((item) => {
+      item.disabled = false;
+    });
   };
   removeDisabled(infoAboutAdForm);
   removeDisabled(filtersForm);
 };
 
-export {addFormsLocking, removeFormsLocking};
+const addFormValidation = () => {
+  const MIN_TITLE_LENGTH = 30;
+  const MAX_TITLE_LENGTH = 100;
+  const MAX_PRICE = 1000000;
+  const adTitle = document.querySelector('#title');
+  const adPrice = document.querySelector('#price');
+
+  adTitle.addEventListener('input', () => {
+    const valueLength = adTitle.value.length;
+
+    if (valueLength < MIN_TITLE_LENGTH) {
+      adTitle.setCustomValidity(`Имя должно состоять минимум из 30-х символов. Еще ${ MIN_TITLE_LENGTH - valueLength } символов.`);
+    } else if (valueLength > MAX_TITLE_LENGTH) {
+      adTitle.setCustomValidity(`Имя не должно превышать 100 символов. Удалите ${ valueLength - MAX_TITLE_LENGTH} символов.`);
+    } else {
+      adTitle.setCustomValidity('');
+    }
+  });
+
+  adPrice.addEventListener('input', () => {
+    if (adPrice.value < 0) {
+      adPrice.setCustomValidity('Значение не должно быть отрицательным');
+    } else if (adPrice.value > MAX_PRICE) {
+      adPrice.setCustomValidity(`Значение не должно быть больше ${MAX_PRICE}`);
+    } else {
+      adPrice.setCustomValidity('');
+    }
+  });
+};
+
+export {addFormsLocking, removeFormsLocking, addFormValidation};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {createSimilarAds} from './data-generator.js';
 import {createAd} from './creating-ads.js';
-import {addFormsLocking, removeFormsLocking} from './form-processing.js';
+import {addFormsLocking, removeFormsLocking, addFormValidation} from './form-processing.js';
 
 const SIMILAR_AD_COUNT = 10;
 const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map((element, index) => createSimilarAds(element, index));
@@ -9,5 +9,6 @@ const map = document.querySelector('#map-canvas');
 
 map.appendChild(createAd(similarAds[0]));
 
-addFormsLocking();
+addFormsLocking;
 removeFormsLocking;
+addFormValidation();
