120c26c5c0528ed55e5c0008b523215d6bbf2772
ZKseniya <zaharina.ksenia@yandex.ru>
2021-07-10 15:59:28 +0300

    feat: добавляет синхронизацию полей 'количество комнат - количество гостей'

--- js/form-processing.js
+++ js/form-processing.js
@@ -28,19 +28,24 @@ const removeFormsLocking = () => {
 };
 
 const addFormValidation = () => {
-  const MIN_TITLE_LENGTH = 30;
-  const MAX_TITLE_LENGTH = 100;
+  const TitleLength = {
+    MIN : 30,
+    MAX : 100,
+  };
   const MAX_PRICE = 1000000;
   const adTitle = document.querySelector('#title');
   const adPrice = document.querySelector('#price');
+  const roomCapacitySelect = document.querySelector('#room_number');
+  const guestCapacitySelect = document.querySelector('#capacity');
+  const guestCapacityOptions = guestCapacitySelect.querySelectorAll('option');
 
   adTitle.addEventListener('input', () => {
     const valueLength = adTitle.value.length;
 
-    if (valueLength < MIN_TITLE_LENGTH) {
-      adTitle.setCustomValidity(`Имя должно состоять минимум из 30-х символов. Еще ${ MIN_TITLE_LENGTH - valueLength } символов.`);
-    } else if (valueLength > MAX_TITLE_LENGTH) {
-      adTitle.setCustomValidity(`Имя не должно превышать 100 символов. Удалите ${ valueLength - MAX_TITLE_LENGTH} символов.`);
+    if (valueLength < TitleLength.MIN) {
+      adTitle.setCustomValidity(`Имя должно состоять минимум из 30-х символов. Еще ${ TitleLength.MIN - valueLength } символов.`);
+    } else if (valueLength > TitleLength.MAX) {
+      adTitle.setCustomValidity(`Имя не должно превышать 100 символов. Удалите ${ valueLength - TitleLength.MAX } символов.`);
     } else {
       adTitle.setCustomValidity('');
     }
@@ -55,6 +60,51 @@ const addFormValidation = () => {
       adPrice.setCustomValidity('');
     }
   });
+
+  const addSyncSelects = (evt) => {
+    const currentOption = evt.target;
+    const currentOptionValue = parseFloat(currentOption.value);
+
+    const addDisabledOptions = (optionsList) => {
+      optionsList.forEach((option) => {
+        option.selected = false;
+        const optionValue = parseFloat(option.value);
+        if (optionValue > currentOptionValue || optionValue === 0) {
+          option.disabled = true;
+        } else {
+          option.disabled = false;
+          if (optionValue === 1) {
+            option.selected = true;
+          }
+        }
+      });
+    };
+
+    switch (currentOptionValue) {
+      case 1:
+        addDisabledOptions(guestCapacityOptions);
+        break;
+      case 2:
+        addDisabledOptions(guestCapacityOptions);
+        break;
+      case 3:
+        addDisabledOptions(guestCapacityOptions);
+        break;
+      case 100 || 0:
+        guestCapacityOptions.forEach((option) => {
+          const optionValue = parseFloat(option.value);
+          if (optionValue !== 0) {
+            option.disabled = true;
+            option.selected = false;
+          } else {
+            option.disabled = false;
+            option.selected = true;
+          }
+        });
+    }
+  };
+
+  roomCapacitySelect.addEventListener('change', (evt) => addSyncSelects(evt));
 };
 
 export {addFormsLocking, removeFormsLocking, addFormValidation};
